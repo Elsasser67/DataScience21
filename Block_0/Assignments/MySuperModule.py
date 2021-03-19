d6176d2dd91247f86a1d6458d23b1e27edf19b78
Elsasser67 <theo.schnell67@gmail.com>
2021-03-19 22:03:48 +0100

    Add functions

--- Block_0/Assignments/MySuperModule.py
+++ Block_0/Assignments/MySuperModule.py
@@ -1,13 +1,24 @@
 class ListKeeper:
+    pass
     
     def __init__(self):
         self.Keeper = {"example" : [1, 2, 3, 4, 5]}
         
-    def show():
+    def show(self):
+        nb = 0
+        for i,j in self.Keeper.items():
+            nb+=1
+            print("Name of List ", nb, " :")
+            print(i)
+
+    def add(self,name, list):
+        self.Keeper[name] = list
+
+    def delete(self, name):
+        del self.Keeper[name]
         
-    def add(name, list):
-        
-    def delete():
-        
-        ploud
-        
+    def sort(self,name):
+        self.Keeper
+
+    def append(self,name, list):
+        pass
