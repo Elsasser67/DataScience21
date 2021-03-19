f349f602b12ab51d7dd21eb79c08481227f50eb5
Elsasser67 <theo.schnell67@gmail.com>
2021-03-19 18:33:37 +0100

    Nouveau Module

---
+++ Block_0/Assignments/MySuperModule.py
@@ -0,0 +1,11 @@
+class ListKeeper:
+    
+    def __init__(self):
+        self.Keeper = {"example" : [1, 2, 3, 4, 5]}
+        
+    def show():
+        
+    def add(name, list):
+        
+    def delete():
+        
