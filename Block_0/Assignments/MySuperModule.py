897ad6ec797a70819c28c651f37f074c31c7b247
Elsasser67 <theo.schnell67@gmail.com>
2021-03-20 00:17:32 +0100

    Comments

--- Block_0/Assignments/MySuperModule.py
+++ Block_0/Assignments/MySuperModule.py
@@ -1,26 +1,30 @@
 class ListKeeper:
-    pass
     
     def __init__(self):
+        '''Initalization'''
         self.Keeper = {"example" : [1, 2, 3, 4, 5]}
         
     def show(self):
+        '''Show every name of the lists'''
         print("Lists :")
         for i in self.Keeper:
             print("- ", i)
 
     def add(self, name, list):
+        '''Add a new list'''
         self.Keeper[name] = list
 
     def delete(self, name):
+        '''Delete the list'''
         del self.Keeper[name]
         
     def sort(self, name):
+        '''Sort the list'''
         sortedList = self.Keeper[name]
         sortedList.sort()
         print(sortedList)
         
     def append(self, name, list):
+        '''Appends two lists to have one list at the end'''
         newList = self.Keeper[name]
         newList.extend(list)
-        print(newList)
